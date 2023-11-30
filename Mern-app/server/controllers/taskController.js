4d1553de72885d2462ce18412143ea4a4cf87a64
ShaikMohammedGouse <106924230+ShaikMohammedGouse@users.noreply.github.com>
2023-11-30 21:44:33 +0530

    Add files via upload

---
+++ Mern-app/server/controllers/taskController.js
@@ -0,0 +1,126 @@
+const User = require('../../database/model/user.model');
+const Task = require('../../database/model/task.model');
+
+const addTask = async (req, res) => {
+	const { task, id } = req.body;
+
+	try {
+		if (!task) return res.status(400).send('please enter the task');
+		// if (task.length < 10) res.status(400).send('add minimum 10 char');
+		const taskDetail = await new Task({
+			task,
+			cretedBy: id,
+		});
+		await taskDetail.save();
+		return res.status(200).send(taskDetail);
+	} catch (error) {
+		return res.status(400).send('task addition failed');
+	}
+};
+
+// const addTask = async (req, res) => {
+// 	const { task, id } = req.body;
+  
+// 	try {
+// 	  if (!task) return res.status(400).send('please enter the task');
+// 	  if (task.length < 10) return res.status(400).send('add minimum 10 char'); // Add return here
+// 	  const taskDetail = await new Task({
+// 		task,
+// 		createdBy: id,
+// 	  });
+// 	  await taskDetail.save();
+// 	  return res.status(200).send(taskDetail);
+// 	} catch (error) {
+// 	  return res.status(400).send('task addition failed');
+// 	}
+	
+//   };
+  
+const getAllTasks = async (req, res) => {
+	const { id } = req.query;
+	try {
+		let tasklist = await Task.find({ cretedBy: id });
+		console.log('Fetched tasks:', tasklist);
+		return res.status(200).send(tasklist);
+	} catch (error) {
+		return res.status(400).send(error);
+	}
+};
+
+// In taskController.js
+const editTask = async (req, res) => {
+	const { id } = req.params;
+	const { updatedTask } = req.body;
+  
+	try {
+	  // Find the task by ID and update its details
+	  let task = await Task.findByIdAndUpdate(id, { task: updatedTask }, { new: true });
+  
+	  if (!task) {
+		return res.status(404).send('Task not found');
+	  }
+  
+	  return res.status(200).send(task);
+	} catch (error) {
+	  console.error(error);
+	  return res.status(500).send('Internal Server Error');
+	}
+  };
+
+
+const statusChange = async (req, res) => {
+	const { id, string } = req.body;
+
+	try {
+		let task = await Task.findById({ _id: id });
+		if (string === 'right') {
+			if (task.status === 'backlog') {
+				task.status = 'todo';
+				task.save();
+				return res.send(task);
+			} else if (task.status === 'todo') {
+				task.status = 'doing';
+				task.save();
+				return res.send(task);
+			} else if (task.status === 'doing') {
+				task.status = 'done';
+				task.save();
+				return res.send(task);
+			}
+		} else {
+			if (task.status === 'done') {
+				task.status = 'doing';
+				task.save();
+				return res.send(task);
+			} else if (task.status === 'doing') {
+				task.status = 'todo';
+				task.save();
+				return res.send(task);
+			} else if (task.status === 'todo') {
+				task.status = 'backlog';
+				task.save();
+				return res.send(task);
+			}
+		}
+	} catch (error) {}
+};
+
+const deleteTask = async (req, res) => {
+	const { id } = req.params;
+	try {
+		let response = await Task.findByIdAndDelete(id);
+		return res.status(200).send(response);
+	} catch (error) {
+		return res.status(400).send('deleteFailed');
+	}
+};
+
+module.exports = {
+	addTask,
+	getAllTasks,
+	editTask,
+	statusChange,
+	deleteTask,
+};
+  
+
